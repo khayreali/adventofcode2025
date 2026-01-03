f3827566bcce6865256e8f021c35deaa8f9171d7
Khayre <khayreahmed@gmail.com>
2026-01-02 20:12:07 -0500

    .

--- day3.py
+++ day3.py
@@ -3,14 +3,27 @@ from utils import read_input
 puzzle = read_input(3)
 bank_list = puzzle.split("\n")
 
-def calculate_joltage(bank):
-    max_joltage = 0
-    for i in range(len(bank)):
-        for j in range(i + 1, len(bank)):
-            joltage = int(bank[i] + bank[j])
-            if joltage > max_joltage:
-                max_joltage = joltage
-    return max_joltage
+
+def calculate_joltage(bank, num_digits=12):
+    n = len(bank)
+    result = []
+    start = 0
+    
+    for i in range(num_digits):
+        digits_remaining_to_pick = num_digits - i
+        end = n - digits_remaining_to_pick
+        
+        max_digit = '0'
+        max_pos = start
+        for pos in range(start, end + 1):
+            if bank[pos] > max_digit:
+                max_digit = bank[pos]
+                max_pos = pos
+        
+        result.append(max_digit)
+        start = max_pos + 1
+    
+    return int(''.join(result))
 
 
 def main():
