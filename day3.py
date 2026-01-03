bb0f87d763da71b39c94bab8b30c33ec40481feb
Khayre <khayreahmed@gmail.com>
2026-01-02 20:05:06 -0500

    .

--- day3.py
+++ day3.py
@@ -1,16 +1,9 @@
 from utils import read_input
 
 puzzle = read_input(3)
-bank_list = []
-start_idx = 0
-
-for idx, num in enumerate(puzzle):
-    if num == "\n":
-        bank_list.append(puzzle[start_idx:idx]) if start_idx == 0 else bank_list.append(puzzle[start_idx+1:idx])
-        start_idx = idx
+bank_list = puzzle.split("\n")
 
 def calculate_joltage(bank):
-    check_set = set()
     max_joltage = 0
     for i in range(len(bank)):
         for j in range(i + 1, len(bank)):
