3bc54e7d0495a744b78d32d9abbc0ead6a72f1ca
Khayre <khayreahmed@gmail.com>
2026-01-05 19:34:42 -0500

    traverse graph

--- day4.py
+++ day4.py
@@ -11,3 +11,30 @@ for char in puzzle:
         continue
     else:
         tmp_list.append(char)
+
+res = 0
+
+def traverse_graph(graph):
+    ROW = len(graph)
+    COL = len(graph[0])
+
+    # for row in range(ROW):
+    #     for col in range(COL):
+    #         # if check_forklift_access(row, col):
+    #         #     res += 1
+    return res
+
+
+# def check_forklift_access(row, col)
+#     directions = [row-1, row+1, col-1, col+1]
+#     for direction in directions:
+        
+
+
+# def main():
+#     return traverse_graph(puzzle_list)
+
+
+
+# if __name__ == "__main__":
+#     main()
